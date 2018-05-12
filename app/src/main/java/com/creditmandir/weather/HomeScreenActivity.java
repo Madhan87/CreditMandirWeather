3376451b48da1d1b5051c0a07abc7c3a4e6b5bf1
Madhan87 <jmadhanprofile@gmail.com>
2018-05-12 06:55:08 +0530

    Display Dialog
    Added show method to populate dialogbox

--- app/src/main/java/com/creditmandir/weather/HomeScreenActivity.java
+++ app/src/main/java/com/creditmandir/weather/HomeScreenActivity.java
@@ -54,6 +54,7 @@ public class HomeScreenActivity extends AppCompatActivity implements iWeatherCal
                             dialog.cancel();
                         }
                     });
+                    builder.show();
                 }
             });
         }
